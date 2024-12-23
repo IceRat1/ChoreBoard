2fe6eaf89c13eedb7e052e137c3666b4b45c7c95
IceRat1 <44247012+IceRat1@users.noreply.github.com>
2024-12-23 15:31:20 -0500

    Added main chores board and ability to add members to groups

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+import functools
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -14,10 +16,10 @@ bp = Blueprint('board', __name__)
 def index():
     db = get_db()
     groups = db.execute(
-        'SELECT * FROM groups'
-        ' WHERE EXISTS (SELECT userid, groupid' 
-        ' FROM user_groups'
-        ' WHERE (user_groups.groupid = groups.id AND user_groups.userid = ?))',
+        'SELECT g.id, g.title, g.created, u.username FROM user_groups ug'
+        ' JOIN groups g ON ug.groupid = g.id'
+        ' JOIN user u ON ug.userid = u.id'
+        ' WHERE ug.userid = ?',
         (g.user['id'],)
     ).fetchall()
     return render_template('board/index.html', groups=groups)
@@ -53,3 +55,88 @@ def create():
             db.commit()
             return redirect(url_for('board.index'))
     return render_template('board/create.html')
+
+def get_group(id):
+    db = get_db()
+    group = db.execute(
+        'SELECT g.id, title, created'
+        ' FROM groups g'
+        ' WHERE g.id = ?',
+        (id,)
+    ).fetchone()
+
+    if group is None:
+        abort(404, f"Group id {id} doesn't exist.")
+    
+    ug = db.execute(
+        'SELECT * FROM user_groups ug'
+        ' WHERE ug.userid = ? AND ug.groupid = ?',
+        (g.user['id'], id)
+    ).fetchone()
+
+    if ug is None:
+        abort(403)
+    
+    return group
+
+@bp.route('/<int:id>/home', methods=('GET','POST'))
+@login_required
+def home(id):
+    db = get_db()
+    group = get_group(id)
+    chores = db.execute(
+        'SELECT c.id, c.title, c.created, c.reward, u.username FROM user_chores uc'
+        ' JOIN chores c ON uc.choreid = c.id'
+        ' JOIN user u ON uc.userid = u.id'
+        ' WHERE u.id = ? AND c.group_id = ?',
+        (g.user['id'], id)
+    ).fetchall()
+    members = db.execute(
+        'SELECT u.id, u.username FROM user_groups ug'
+        ' JOIN user u ON ug.userid = u.id'
+        ' JOIN groups g ON ug.groupid = g.id'
+        ' WHERE ug.groupid = ?',
+        (id,)
+    ).fetchall()
+
+
+    return render_template('board/home.html', group=group, chores=chores, members=members)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_group(id)
+    db = get_db()
+    db.execute('DELETE FROM groups WHERE id = ?', (id,))
+    db.execute('DELETE FROM user_groups WHERE user_groups.groupid = ?', (id,))
+    db.commit()
+    return redirect(url_for('board.index'))
+
+@bp.route('/<int:id>/add', methods=('GET', 'POST'))
+@login_required
+def add(id):
+    if request.method == 'POST':
+        db = get_db()
+        userid = request.form['userid']
+        error = None
+
+        if not userid:
+            error = "Please enter an id."
+        elif db.execute('SELECT * from user u WHERE u.id = ?', (userid,)).fetchone() is None:
+            error = "That user doesn't exist."
+        else:
+            try:
+                db.execute(
+                    "INSERT INTO user_groups (userid, groupid) VALUES (?, ?)",
+                    (userid, id)
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {userid} is already in the group."
+            else:
+
+                return redirect(url_for("board.home", id=id))
+        flash(error)
+    
+    return render_template('board/add.html')
+
--- schema.sql
+++ schema.sql
@@ -19,8 +19,29 @@ CREATE TABLE groups (
 CREATE TABLE user_groups (
     userid INTEGER NOT NULL,
     groupid INTEGER NOT NULL,
+    score INTEGER NOT NULL DEFAULT 0,
     PRIMARY KEY (userid, groupid),
     FOREIGN KEY (userid) REFERENCES user (id),
     FOREIGN KEY (groupid) REFERENCES groups (id)
 );
+
+CREATE TABLE chores (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    author_id INTEGER NOT NULL,
+    group_id INTEGER NOT NULL,
+    reward INTEGER NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES user (id),
+    FOREIGN KEY (group_id) REFERENCES groups (id)
+);
+
+CREATE TABLE user_chores (
+    userid INTEGER NOT NULL,
+    choreid INTEGER NOT NULL,
+    PRIMARY KEY (userid, choreid),
+    FOREIGN KEY (userid) REFERENCES user (id),
+    FOREIGN KEY (choreid) REFERENCES chores (id)
+)
+
 
